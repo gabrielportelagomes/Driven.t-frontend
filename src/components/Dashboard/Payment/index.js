32616bfc4f15405c6c8db3f23848cab0a57965f8
sabta <alencarsabta@gmail.com>
2023-03-01 20:00:36 -0400

    feat: confirmando pagamento

--- src/components/Dashboard/Payment/index.js
+++ src/components/Dashboard/Payment/index.js
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { BsFillCheckCircleFill } from 'react-icons/bs';
+import styled from 'styled-components';
 import useTicketType from '../../../hooks/api/useTicketType';
 import Button from './Button';
 import PaymentContainer from './PaymentContainer';
@@ -38,18 +40,55 @@ export default function TicketAndPayment() {
     <>
       <PaymentContainer>
         <h1>Ingresso e Pagamento</h1>
+
         {!confirmedTicket ? (
           <>
             <p>Primeiro, escolha sua modalidade de ingresso</p>
             <div>{ticketTypeModality}</div>
-            {(!ticketType.id && '') || (ticketType.isRemote && <Summary ticket={ticketType} setConfirmedTicket={setConfirmedTicket} />) || (ticketType.isRemote === false && <>
-              <p style={{ margin: '44px 0 17px 0' }}>Ótimo, Agora escolha sua modalidade de hospedagem</p> <PresencialModality ticketTypes={ticketTypes} setConfirmedTicket={setConfirmedTicket}/> </> )}
+            {(!ticketType.id && '') ||
+              (ticketType.isRemote && <Summary ticket={ticketType} setConfirmedTicket={setConfirmedTicket} />) ||
+              (ticketType.isRemote === false && (
+                <>
+                  <p style={{ margin: '44px 0 17px 0' }}>Ótimo, Agora escolha sua modalidade de hospedagem</p>{' '}
+                  <PresencialModality ticketTypes={ticketTypes} setConfirmedTicket={setConfirmedTicket} />{' '}
+                </>
+              ))}
           </>
         ) : (
           '' //tela de pagamento
         )}
-
       </PaymentContainer>
+
+      <PaymentConfirm>
+        <Icon />
+        <Box>
+          <p>Pagamento confirmado!</p>
+          <span>Prossiga para escolha de hospedagem e atividades</span>
+        </Box>
+      </PaymentConfirm>
     </>
   );
 }
+
+const PaymentConfirm = styled.div`
+  display: flex;
+  margin-top: 25px;
+`;
+const Icon = styled(BsFillCheckCircleFill)`
+  color: #36b853;
+  font-size: 48px;
+`;
+const Box = styled.div`
+  margin-left: 10px;
+  & > p {
+    font-weight: 700;
+    font-size: 18.8px;
+    margin-bottom: 5px;
+    color: #454545;
+  }
+  & > span {
+    font-size: 17.5px;
+    font-weight: 1;
+    color: #454545;
+  }
+`;
