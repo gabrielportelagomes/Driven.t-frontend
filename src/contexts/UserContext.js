615399059f74efdc50160cfec8187df552d7d6bb
sabta <alencarsabta@gmail.com>
2023-03-06 22:06:27 -0400

    feat: hotel resume

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { createContext } from 'react';
 
 import useLocalStorage from '../hooks/useLocalStorage';
@@ -7,9 +8,10 @@ export default UserContext;
 
 export function UserProvider({ children }) {
   const [userData, setUserData] = useLocalStorage('userData', {});
-  
+  const [confirmedPayment, setConfirmedPayment] = useState(null);
+
   return (
-    <UserContext.Provider value={{ userData, setUserData }}>
+    <UserContext.Provider value={{ userData, setUserData, confirmedPayment, setConfirmedPayment }}>
       {children}
     </UserContext.Provider>
   );
