d431721fd42d443b8f839e6ad9b6f8d9d5330451
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-03-13 22:17:08 -0300

    feat: unconfirmed payment message

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,3 +1,57 @@
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import usePayment from '../../../hooks/api/usePayment';
+import useTicket from '../../../hooks/api/useTicket';
+
 export default function Activities() {
-  return 'Atividades: Em breve!';
+  const { ticket } = useTicket();
+  const { getPayment } = usePayment();
+  const [confirmedPayment, setConfirmedPayment] = useState(false);
+
+  useEffect(() => {
+    if (ticket) {
+      getPayment(ticket.id)
+        .then((response) => {
+          setTimeout(() => setConfirmedPayment(true), 0);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [ticket]);
+
+  if (!confirmedPayment) {
+    return (
+      <EmptyContainer>
+        <h1>Escolha de atividades</h1>
+        <div>
+          <p style={{ width: '480px' }}>Você precisa ter confirmado pagamento antes de fazer a escolha de atividades</p>
+        </div>
+      </EmptyContainer>
+    );
+  }
+
+  return <></>;
 }
+
+const EmptyContainer = styled.div`
+  height: 100%;
+  & > div {
+    height: 90%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 auto;
+    & > p {
+      width: 420px;
+      color: #8e8e8e;
+      font-size: 20px;
+      font-weight: 400;
+      text-align: center;
+    }
+  }
+  & > h1 {
+    font-size: 34px;
+    font-weight: 400;
+  }
+`;
